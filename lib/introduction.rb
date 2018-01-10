702b6b30849df05182e0d0e02e46e0bd22a6ca98
Felipe Leon <felipeleon92@gmail.com>
2018-01-10 18:00:54 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,9 @@
-# The method below is programmed to accept an argument
+# The method below is programmed to accept an argument and insert it into the string with the #{} syntax.
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+# The method below is programmed to accept TWO arguments and insert it into the string with the #{} syntax.
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
